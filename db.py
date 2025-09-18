7f478c777aee76846ea0d60bf85373e7af380871
Maxime Moreillon <m.moreillon@gmail.com>
2025-09-18 19:50:16 +0900

    update, delete

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 from beanie import init_beanie
 from dotenv import load_dotenv
 from os import getenv
-from pymongo import AsyncMongoClient
 from models.items import Item
+
+# from pymongo import AsyncMongoClient
 
 load_dotenv()
 
@@ -11,7 +12,12 @@ MONGODB_URL = getenv("MONGODB_URL", "mongodb://localhost:27017/fastapi-beanie-cr
 
 
 async def init():
-    client = AsyncMongoClient(MONGODB_URL)
 
-    # Init beanie with the Product document class
-    await init_beanie(database=client.db_name, document_models=[Item])
+    # client = AsyncMongoClient(
+    #     "mongodb://user:pass@host:27017"
+    # )
+
+    # await init_beanie(database=client.db_name, document_models=[Item])
+
+    # TODO: figure out if this isn't better
+    await init_beanie(connection_string=MONGODB_URL, document_models=[Item])
